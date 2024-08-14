4e6271b5de987f52edd4c79a18c354330477ba64
akhilmidda18 <65309108+akhilmidda18@users.noreply.github.com>
2024-08-14 14:38:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 import joblib
+import numpy as np
 
 app = Flask(__name__)
 
@@ -12,8 +13,23 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    features = [float(x) for x in request.form.values()]
-    prediction = model.predict([features])
+    # Get data from form
+    pregnancies = float(request.form['pregnancies'])
+    glucose = float(request.form['glucose'])
+    blood_pressure = float(request.form['bloodPressure'])
+    skin_thickness = float(request.form['skinThickness'])
+    insulin = float(request.form['insulin'])
+    bmi = float(request.form['bmi'])
+    diabetes_pedigree_function = float(request.form['diabetesPedigreeFunction'])
+    age = float(request.form['age'])
+
+    # Prepare the feature vector for prediction
+    features = np.array([[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, age]])
+
+    # Make prediction
+    prediction = model.predict(features)
+
+    # Return result
     return render_template('index.html', prediction_text=f'Prediction: {prediction[0]}')
 
 if __name__ == '__main__':
